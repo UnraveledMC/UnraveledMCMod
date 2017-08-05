49c2b7cc4a3defed9cf9bd484eab4a9b58399159
IvanTheOven <codygithub18@gmail.com>
2017-08-05 12:29:15 -0400

    some /pinch cmd
    lionnco told me to add this :(

---
+++ src/main/java/me/unraveledmc/unraveledmcmod/command/Command_pinch.java
@@ -0,0 +1,31 @@
+package me.unraveledmc.unraveledmcmod.command;
+
+import me.unraveledmc.unraveledmcmod.rank.Rank;
+import me.unraveledmc.unraveledmcmod.util.FUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+@CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Pinch a bitch.", usage = "/<command> <player>")
+public class Command_pinch extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            return false;
+        }
+        Player player = getPlayer(args[0]);
+        if (player == null)
+        {
+            msg(FreedomCommand.PLAYER_NOT_FOUND);
+            return true;
+        }
+        FUtil.bcastMsg(ChatColor.AQUA + plugin.esb.getDisplayName(playerSender.getName()) + ChatColor.AQUA + " has fucking pinched " + plugin.esb.getDisplayName(player.getName()));
+        return true;
+    }
+}
