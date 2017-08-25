190e3c289d453d7f11bbefab2f48d294c86b7d49
IvanTheOven <codygithub18@gmail.com>
2017-08-25 14:29:29 -0400

    undo gender change thing

--- src/main/java/me/unraveledmc/unraveledmcmod/command/Command_ban.java
+++ src/main/java/me/unraveledmc/unraveledmcmod/command/Command_ban.java
@@ -79,7 +79,7 @@ public class Command_ban extends FreedomCommand
 
         if (player != null)
         {
-            FUtil.bcastMsg(player.getName() + " has been a VERY naughty, naughty player.", ChatColor.RED);
+            FUtil.bcastMsg(player.getName() + " has been a VERY naughty, naughty boy.", ChatColor.RED);
         }
 
         // Ban player
