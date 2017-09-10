4d3a2b8c86fed41e4872271bb862794115cfae3a
CreeperSeth <13510767+ZeroEpoch1969@users.noreply.github.com>
2017-09-09 23:19:58 -0700

    Tired of getting hit from this

--- src/main/java/me/unraveledmc/unraveledmcmod/command/Command_denick.java
+++ src/main/java/me/unraveledmc/unraveledmcmod/command/Command_denick.java
@@ -7,14 +7,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.MOD, source = SourceType.BOTH)
-@CommandParameters(description = "Essentials Interface Command - Remove the nickname of all players on the server.", usage = "/<command>")
+@CommandParameters(description = "Essentials Interface Command - Remove the nickname of all non-staff or all players on the server.", usage = "/<command> [-a]")
 public class Command_denick extends FreedomCommand
 {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        FUtil.staffAction(sender.getName(), "Removing all nicknames", false);
+        Boolean includeStaff = false;
+        if (args.length > 0 && args[0].equals("-a"))
+        {
+            includeStaff = true;
+        }
+    
+        FUtil.staffAction(sender.getName(), "Removing nicknames for all " + (includeStaff ? "players" : "non-staff"), false);
 
         for (Player player : server.getOnlinePlayers())
         {
